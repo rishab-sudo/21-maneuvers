93e1b524daba21af30beceb97a01538a08b0d3ba
rishab-sudo <rishabdutt4@gmail.com>
2025-05-26 18:05:12 +0530

    26may25

--- src/Pages/ServiceTypes/PerformanceService.js
+++ src/Pages/ServiceTypes/PerformanceService.js
@@ -4,44 +4,93 @@ import LetsTalk from "../../Components/LetsTalk"
 import "./PerformanceService.css"
 
 const ContentService = () => {
-   const images = [
-    require('../../assets/grain-town-digital-Post-design.png'),
-     require('../../assets/grain-town-digital-Post-design.png'),
-         require('../../assets/grain-town-digital-Post-design.png'),
-         require('../../assets/grain-town-digital-Post-design.png'),
-                 require('../../assets/grain-town-digital-Post-design.png'),
-                     require('../../assets/grain-town-digital-Post-design.png'),
-                         require('../../assets/grain-town-digital-Post-design.png'),
-                             require('../../assets/grain-town-digital-Post-design.png'),
+
+    const data = [
+    { amount: '₹2,50,000', label: 'Total banner Spend' },
+    { amount: '₹1,80,000', label: 'Facebook banners' },
+    { amount: '₹70,000', label: 'Google banners' },
+    { amount: '₹40,000', label: 'Instagram banners' },
+    { amount: '₹30,000', label: 'YouTube banners' },
+    { amount: '₹1,20,000', label: 'Affiliate Marketing' },
   ];
+
+
   return (
    <>
     <Container  fluid className='serviceType-banner-section'>
 <Container className='serviceType-banner-container'>
-<div className='serviceType-banner-heading-div'>
+<div className='serviceType-banner-hebannering-div'>
   <h1>Performance Marketing</h1>
   <p>YOUR BRAND DESERVES MORE THAN VISIBILITY, IT DESERVES VELOCITY.</p>
 <p>We don’t just take you online. We position you precisely where your audience scrolls, clicks, and connects. With sharp strategy and standout creativity, 21 Maneuvers navigates the digital maze, turning presence into performance.
 </p>
   <p>FUELING RESULTS, NOT JUST REACH</p>
 </div>
-<img className='mt-2 performance-banner-img' src={require("../../assets/social-media-creative-design.png")} alt="content-service-banner"/>
+
+
+ <div className="performance-boxes-container">
+      {data.map((item, index) => (
+        <div key={index} className="performance-banner-box">
+          <div className="performance-amount">{item.amount}</div>
+          <div className="performance-label">{item.label}</div>
+        </div>
+      ))}
+    </div>
 </Container>
 </Container>
 
-  <Container fluid className='serviceType-images-section'>
-      <Container fluid className='serviceType-content-container'>
-       <Row className="serviceType-row">
-  {images.map((imgSrc, index) => (
-    <Col xs={12} md={6} key={index} className='p-0 g-0'>
-      <div className="serviceType-image-wrapper">
-        <img className="serviceType-grid-img"src={imgSrc} alt={`Post ${index + 1}`} />
-      </div>
-    </Col>
-  ))}
-</Row>
+<Container fluid>
+  <Container className='performance-platform-container'>
+    <div className='performance-platform-box'>
+<div className=''>
+  <img src={require("../../assets/coffee_icon.png")} alt=""/>
+</div>
+<div>
+  <p>Google Ads</p>
+    <p> rs 10,0000</p>
+      <p>1800 Leads</p>
+        <p> 520 Connections</p>
+</div>
+</div>
+
+ <div className='performance-platform-box'>
+<div className=''>
+  <img src={require("../../assets/coffee_icon.png")} alt=""/>
+</div>
+<div>
+  <p>Google Ads</p>
+    <p> rs 10,0000</p>
+      <p>1800 Leads</p>
+        <p> 520 Connections</p>
+</div>
+</div>
+
+ <div className='performance-platform-box'>
+<div className=''>
+  <img src={require("../../assets/coffee_icon.png")} alt=""/>
+</div>
+<div>
+  <p>Google Ads</p>
+    <p> rs 10,0000</p>
+      <p>1800 Leads</p>
+        <p> 520 Connections</p>
+</div>
+</div>
+
+ <div className='performance-platform-box'>
+<div className=''>
+  <img src={require("../../assets/coffee_icon.png")} alt=""/>
+</div>
+<div>
+  <p>Google Ads</p>
+    <p> rs 10,0000</p>
+      <p>1800 Leads</p>
+        <p> 520 Connections</p>
+</div>
+</div>
   </Container>
-    </Container>
+   
+</Container>
 
     <Container className='serviceType-descp-section'>
       <Container className='serviceType-descp-content-container'>
@@ -52,22 +101,22 @@ const ContentService = () => {
 <p className='mt-5'>At 21 Maneuvers, performance is more than just numbers, it’s the outcome of strategy, creativity, and flawless execution. We don’t chase vanity metrics. We focus on what truly matters: growth, engagement, and ROI.</p>
 <p>Every campaign is meticulously planned, performance-tracked, and continuously optimized. From audience insights to conversion data, we leverage real-time analytics to fine-tune every move, ensuring your brand achieves measurable, sustainable results.
 </p>
-    <p>BECAUSE WHEN PERFORMANCE LEADS, YOUR BRAND DOESN'T JUST COMPETE; IT DOMINATES.</p>
+    <p>BECAUSE WHEN PERFORMANCE LEbannerS, YOUR BRAND DOESN'T JUST COMPETE; IT DOMINATES.</p>
       </Container>
     </Container>
 
 
-<Container className='elevate-section'>
-<Container className='elevate-content-container'>
-  <div className='elevate-left-side'>
+<Container className='amplify-section'>
+<Container className='amplify-content-container'>
+  <div className='amplify-left-side'>
 <p>Amplify YOUR</p>
 <p>IMPACT</p>
 <p>across </p>
 <p>DIGITAL PLATFORMS.
 </p>
   </div>
-  <div className='elevate-right-side'>
-<p>We merge strategy and creativity to fuel meaningful engagement across digital platforms. Our approach ensures your brand builds a powerful presence where your audience already lives. With data-led performance marketing, we turn insights into action and clicks into conversions. Every move is designed to drive real, measurable growth, consistently and effectively.
+  <div className='amplify-right-side'>
+<p>We merge strategy and creativity to fuel meaningful engagement across digital platforms. Our approach ensures your brand builds a powerful presence where your audience alrebannery lives. With data-led performance marketing, we turn insights into action and clicks into conversions. Every move is designed to drive real, measurable growth, consistently and effectively.
 </p>
   </div>
 </Container>
